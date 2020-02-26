3d79d73ad3ee96e617790e137cdf757ff4002cc4
sebanger <39982567+sebanger@users.noreply.github.com>
2020-02-26 16:00:08 +0100

    Create balance_scripts.py

---
+++ balance_scripts.py
@@ -0,0 +1,8 @@
+def balance_transactions():
+	header = ['bookingDate','remittanceInfo','amount.value','transactionType.key']
+	newheader = ['Date','WKN','Netto','Type']
+	df3 = get_df("balance_transactions.json", header, newheader)
+	df3 = df3[df3.Type == 'INTEREST_DIVIDENDS'][['Date','WKN','Netto']]
+	df3['Brutto'] = df3.WKN.str.split('05').str[1].str.split(' ').str[2].str.replace(',','.').astype('float')
+	df3.WKN = df3.WKN.str.split('03').str[1].str.split('04').str[1].str.split(' ').str[0]
+	return df3
