06dea7eb087f6a65d63081b92ae9b63e22f9e1c5
sebanger <39982567+sebanger@users.noreply.github.com>
2020-02-26 16:41:17 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import datetime
+import sys, os
 from depot_scripts import depot_transactions, depot_positions
 from balance_scripts import balance_transactions
 from requests import main_requests
@@ -7,6 +8,10 @@ def currenttime():
     return datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
 
 def start():
+	currentdir=sys.argv[1]
+	currentdir=currentdir.replace("\\","/")
+	if not os.path.exists('Output'):
+    		os.makedirs('Output')
 	#Personal Information
 	client_id=input("Please enter client_id: ")
 	client_secret=input("Please enter client_secret: ")
@@ -18,12 +23,17 @@ def start():
 	max_transactiondate = input(raw_input("Please enter END of transaction date (otherwise press enter): ") or currenttime())
 	
 	#Get requests calls and several files are saved for further investigation
-	main_requests(client_id, client_secret, username, password)
+	main_requests(client_id, client_secret, username, password, min_transactiondate, max_transactiondate, currentdir)
 
 	#Get data in dataframe for analysis
 	df1 = depot_positions()
 	df2 = depot_transactions()
 	df3 = balance_transactions()
+	
+	#Export dataframes to excel
+	df1.to_excel(currentdir+'/'+"depot_positions"+currenttime+".xlsx")
+	df2.to_excel(currentdir+'/'+"depot_transactions"+currenttime+".xlsx")
+	df3.to_excel(currentdir+'/'+"balance_transactions"+currenttime+".xlsx")
 
 
 if __name__ == "__main__":
