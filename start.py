f609f6e254425980b4a94629df2a23becf28fa06
sebanger <39982567+sebanger@users.noreply.github.com>
2020-02-26 15:56:44 +0100

    Create start.py

---
+++ start.py
@@ -0,0 +1,20 @@
+from currenttime import currenttime
+
+def start():
+	#Personal Information
+	client_id=input("Please enter client_id: ")
+	client_secret=input("Please enter client_secret: ")
+	username = input("Please enter username: ")
+	password = input("Please enter password: ")
+	min_transactiondate = input(raw_input("Please enter START of transaction date (otherwise press enter): ") or currenttime()[:4]"-01-01")
+	max_transactiondate = input(raw_input("Please enter END of transaction date (otherwise press enter): ") or currenttime())
+	main_requests(client_id, client_secret, username, password)
+
+	#Get data in dataframe for analysis
+	df1 = depot_positions()
+	df2 = depot_transactions()
+	df3 = balance_transactions()
+
+
+if __name__ == "__main__":
+	start()
