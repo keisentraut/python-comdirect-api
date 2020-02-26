97d409dfdd2450450671135e426562da99f50eee
sebanger <39982567+sebanger@users.noreply.github.com>
2020-02-26 16:17:42 +0100

    Update start.py

--- start.py
+++ start.py
@@ -12,8 +12,12 @@ def start():
 	client_secret=input("Please enter client_secret: ")
 	username = input("Please enter username: ")
 	password = input("Please enter password: ")
+	
+	#Comdirect has NOT implemented this!
 	min_transactiondate = input(raw_input("Please enter START of transaction date (otherwise press enter): ") or currenttime()[:4]"-01-01")
 	max_transactiondate = input(raw_input("Please enter END of transaction date (otherwise press enter): ") or currenttime())
+	
+	#Get requests calls and several files are saved for further investigation
 	main_requests(client_id, client_secret, username, password)
 
 	#Get data in dataframe for analysis
