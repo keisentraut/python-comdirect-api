ea5039ea21f3e19fa2be164877b27e5e08ebbed1
sebanger <39982567+sebanger@users.noreply.github.com>
2020-02-27 12:57:12 +0100

    Update create_dataframe.py

--- create_dataframe.py
+++ create_dataframe.py
@@ -11,6 +11,10 @@ def get_df(file,header,newheader,outputdir,currenttime):
 	df = json_normalize(tmp.Werte[0])
 	df = df[header]
 	df.columns = newheader
+	if file == "balance_transactions":
+		df = df[df.Type == 'INTEREST_DIVIDENDS'][['Date','WKN','Netto']]
+		df['Brutto'] = df.WKN.str.split('05').str[1].str.split(' ').str[2].str.replace(',','.').astype('float')
+		df.WKN = df.WKN.str.split('03').str[1].str.split('04').str[1].str.split(' ').str[0]
 	df.to_excel(outputdir+'/'+file+'_'+currenttime+'.xlsx',index=False)
 	df['Date'] = pd.to_datetime(df['Date']).dt.date
 	return df
