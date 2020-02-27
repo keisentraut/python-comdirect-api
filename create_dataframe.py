ddb22e232abae60e6e08740ad4f62391d0ff05f1
sebanger <39982567+sebanger@users.noreply.github.com>
2020-02-27 12:42:50 +0100

    Update create_dataframe.py

--- create_dataframe.py
+++ create_dataframe.py
@@ -2,8 +2,8 @@ import pandas as pd
 import json
 from pandas.io.json import json_normalize
 
-def get_df(file,header,newheader):
-	with open(file) as f:
+def get_df(file,header,newheader,outputdir,currenttime):
+	with open(outputdir+'/'+file+'.json') as f:
 		data = json.load(f)
 	tmp = json_normalize(data)
 	tmp = tmp[['values']]
@@ -11,5 +11,6 @@ def get_df(file,header,newheader):
 	df = json_normalize(tmp.Werte[0])
 	df = df[header]
 	df.columns = newheader
-	df['Date'] = pd.to_datetime(df['Date'])
+	df.to_excel(outputdir+'/'+file+'_'+currenttime+'.xlsx',index=False)
+	df['Date'] = pd.to_datetime(df['Date']).dt.date
 	return df
