6181b0c2ed7721c2fac8f56d696a713e5beabf40
sebanger <39982567+sebanger@users.noreply.github.com>
2020-02-27 11:37:26 +0100

    Update depot_scripts.py

--- depot_scripts.py
+++ depot_scripts.py
@@ -1,13 +1,13 @@
 from create_dataframe import get_df
 
-def depot_transactions():
+def depot_transactions(outputdir):
 	header = ['businessDate','quantity.value','instrument.wkn','instrument.shortName','instrument.staticData.instrumentType','executionPrice.value','transactionValue.value']
 	newheader = ['Date','Quantity','WKN','Name','Type','UnitPrice','TotalPrice']
-	df = get_df("depot_transactions.json", header, newheader)
+	df = get_df(outputdir+'/'+"depot_transactions.json", header, newheader)
 	return df
 	
 def depot_positions():
 	header = ['wkn','custodyType','quantity.value','currentPrice.price.value','currentPrice.priceDateTime','purchasePrice.value','currentValue.value','purchaseValue.value']
 	newheader = ['WKN','Type','Quantity','CurrentUnitPrice','Date','PurchaseUnitPrice','CurrentTotalPrice','PurchaseTotalPrice']
-	df2 = get_df("depot_positions.json", header, newheader)
+	df2 = get_df(outputdir+'/'+"depot_positions.json", header, newheader)
 	return df2
