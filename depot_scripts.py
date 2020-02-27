61758ffa0f32261b4f85d4e61ffa84e96b48df70
sebanger <39982567+sebanger@users.noreply.github.com>
2020-02-27 12:42:07 +0100

    Update depot_scripts.py

--- depot_scripts.py
+++ depot_scripts.py
@@ -1,13 +1,13 @@
 from create_dataframe import get_df
 
-def depot_transactions(outputdir):
+def depot_transactions(outputdir,currenttime):
 	header = ['businessDate','quantity.value','instrument.wkn','instrument.shortName','instrument.staticData.instrumentType','executionPrice.value','transactionValue.value']
 	newheader = ['Date','Quantity','WKN','Name','Type','UnitPrice','TotalPrice']
-	df = get_df(outputdir+'/'+"depot_transactions.json", header, newheader)
+	df = get_df("depot_transactions", header, newheader, outputdir,currenttime)
 	return df
 	
-def depot_positions():
+def depot_positions(outputdir,currenttime):
 	header = ['wkn','custodyType','quantity.value','currentPrice.price.value','currentPrice.priceDateTime','purchasePrice.value','currentValue.value','purchaseValue.value']
 	newheader = ['WKN','Type','Quantity','CurrentUnitPrice','Date','PurchaseUnitPrice','CurrentTotalPrice','PurchaseTotalPrice']
-	df2 = get_df(outputdir+'/'+"depot_positions.json", header, newheader)
+	df2 = get_df("depot_positions", header, newheader, outputdir,currenttime)
 	return df2
