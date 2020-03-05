1e4eb7d383c3d3ae7e84e26f1ce7e36d803b0f5f
Klaus Eisentraut <2080545+keisentraut@users.noreply.github.com>
2020-03-05 14:41:57 +0100

    removed old stuff

--- comdirect_api/old/balance_scripts.py
+++
@@ -1,8 +0,0 @@
-from create_dataframe import get_df
-
-
-def balance_transactions(outputdir, currenttime):
-    header = ["bookingDate", "remittanceInfo", "amount.value", "transactionType.key"]
-    newheader = ["Date", "WKN", "Netto", "Type"]
-    df3 = get_df("balance_transactions", header, newheader, outputdir, currenttime)
-    return df3
